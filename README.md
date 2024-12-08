b96d7c20cbd001b6317e42c3e820e28bea307c9b
Aaron Hong <me@aaron.pm>
2024-12-08 06:42:29 -0500

    Added node status

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Shows node information for a node within a Docker swarm.
 The (simple) app has only one environment variable:
 
 - `HOSTNAME` (e.g. `HOSTNAME=test.cluster.shednet.ca`)
+
+### Version
+
+Current version: `1.0.1`.
--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 // deps
 const pkg = require("./package.json");
 var os = require("os");
-const express = require("express")
+const express = require("express");
+const { cp } = require("fs");
+
+// funcs
+
+function getServerInfo() {
+    var hostname = process.env.HOSTNAME || "Unknown";
+    var cpuUsage = os.loadavg()[1] / os.cpus().length * 100;
+    var load = "low";
+
+    if (cpuUsage < 33) {
+        load = "good";
+    } else if (cpuUsage < 66) {
+        load = "marginal";
+    } else {
+        load = "poor";  
+    }
+
+    return { "hostname": hostname, "version": pkg.version, "load": load, "loadStatus": load };
+}
 
 // start server on 3000
 const app = express()
@@ -12,16 +31,21 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.get("/", (req, res) => {
-    var hostname = process.env.HOSTNAME || "Unknown";
+    var srvInfo = getServerInfo();
     // ipv6 isn't supported, so remove the ::ffff: prefix (shows up some times)
     // too lazy to debug
     var ip = (req.headers["CF-Connecting-IP"] || req.headers["x-forwarded-for"] || req.socket.remoteAddress).replace("::ffff:", "");
-    res.render("index", { hostname: hostname, version: pkg.version, userIp: ip });
-  })
+    res.render("index", { loadStatus: srvInfo["loadStatus"].toUpperCase(), hostname: srvInfo["hostname"], version: srvInfo["version"], userIp: ip });
+});
+
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
+
 
 app.listen(port, () => {
     console.log("Listening on " + port)
-})
+});
 
 
 
